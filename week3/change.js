4920042b27960a27f115171257c0bf9652f5caac
Anya Glowa-Kollisch <anyagk@gmail.com>
2016-07-16 15:22:42 -0400

    change algo solved

---
+++ week3/change.js
@@ -0,0 +1,45 @@
+process.stdin.setEncoding('utf8');
+
+var data = ""
+
+function change(n) {
+  var totalNumberOfCoins = 0
+  var workingTotal = 0
+  while (n % 10 < n) {
+    workingTotal = n - (n % 10)
+    totalNumberOfCoins += workingTotal/10
+    n = n % 10
+  }
+  while (n % 5 < n) {
+    workingTotal = n - (n % 5)
+    totalNumberOfCoins += workingTotal/5
+    n = n % 5
+  }
+  totalNumberOfCoins += n
+  return totalNumberOfCoins
+}
+
+process.stdin.on('end', function() {
+  var n = parseInt(data)
+  console.log(change(n))
+  process.exit()
+})
+
+process.stdin.on('readable', function(){
+  new_data = process.stdin.read();
+  if (new_data !== null) {
+   data = data + new_data
+  }
+})
+
+  while (capacity > 0) {
+    for(var i = 0; i < knapsackIngredientArray.length; ++i) {
+      if (capacity >= knapsackIngredientArray[i].weight) {
+        capacity -= knapsackIngredientArray[i].weight
+        finalCalorie += knapsackIngredientArray[i].calorie
+      } else {
+        finalCalorie += knapsackIngredientArray[i].caloriesPerUnit * capacity
+        capacity = 0
+      }
+    }
+  }
